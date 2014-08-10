60517327cda476578e8ae9e2efa4c4883e81ed58
Leonardo Borges <leonardoborges.rj@gmail.com>
2014-08-10 15:13:50 +1000

    Finish parser

--- src/main/scala/challenge/Parser.scala
+++ src/main/scala/challenge/Parser.scala
@@ -230,7 +230,7 @@ object Parser {
    */
   def natural: Parser[Int] =
     Parser { input =>
-      list(digit).map(_.mkString.toInt).run(input)
+      list1(digit).map(_.mkString.toInt).run(input)
     }
 
   /**
@@ -247,7 +247,7 @@ object Parser {
    *  = Fail(UnexpectedInput(h))
    */
   def space: Parser[Char] =
-    ???
+    satisfy(_.isSpaceChar)
 
   /**
    * Return a parse that produces one of more space characters
@@ -265,7 +265,7 @@ object Parser {
    *
    */
   def spaces1: Parser[String] =
-    ???
+    list1(space).map(_.mkString)
 
   /**
    * Return a parser that produces a lower-case character but fails if
@@ -281,7 +281,7 @@ object Parser {
    *  = Fail(UnexpectedInput(H))
    */
   def lower: Parser[Char] =
-    ???
+    satisfy(_.isLower)
 
   /**
    * Return a parser that produces an upper-case character but fails if
@@ -297,7 +297,7 @@ object Parser {
    *  = Fail(UnexpectedInput(h))
    */
   def upper: Parser[Char] =
-    ???
+    satisfy(_.isUpper)
 
   /**
    * Return a parser that produces an alpha character but fails if
@@ -313,36 +313,83 @@ object Parser {
    *  = Fail(UnexpectedInput(?))
    */
   def alpha: Parser[Char] =
-    ???
+    lower ||| upper
 
   /**
    * Return a parser that sequences the given list of parsers by
    * producing all their results but fails on the first failing
    * parser of the list.
    *
-   * scala> Parser.sequence(List(Parser.char, Parser.char, Parser.char)).run("hello")
-   *  = Ok(ParseState(lo,hel))
-   *
-   * scala> Parser.sequence(List(Parser.char, Parser.fail(NotEnoughInput), Parser.char)).run("hello")
+   * scala> Parser.sequence(List(Parser.character, Parser.character, Parser.character)).run("hello")
+   *  = Ok(ParseState(lo,hel)) Errata(should be Ok(ParseState(lo,List(l, e, h))))
+   *                                                         >spelling<
+   * scala> Parser.sequence(List(Parser.character, Parser.failed(NotEnoughInput), Parser.character)).run("hello")
    *  = Fail(NotEnoughInput)
    */
+
+//   errata: as it is, you'll see the following error:
+
+//   scala> Parser.sequence(List(Parser.character, Parser.failed(NotEnoughInput), Parser.character)).run("hello")
+
+// <console>:14: error: no type parameters for method sequence: (parsers: List[challenge.Parser[A]])challenge.Parser[List[A]] exist so that it can be applied to arguments (List[challenge.Parser[_ <: Char]])
+//  --- because ---
+// argument expression's type is not compatible with formal parameter type;
+//  found   : List[challenge.Parser[_ <: Char]]
+//  required: List[challenge.Parser[?A]]
+//               Parser.sequence(List(Parser.character, Parser.failed(NotEnoughInput), Parser.character)).run("hello")
+//                      ^
+
+//   Needs type hint so this works:
+
+//   scala> Parser.sequence(List(Parser.character, (Parser.failed(NotEnoughInput): Parser[Char]), Parser.character)).run("hello")
+
+//   = Fail(NotEnoughInput)
   def sequence[A](parsers: List[Parser[A]]): Parser[List[A]] =
-    ???
+    // using recursion
+    // Parser { input =>
+    //   def parse(cs:String, ps: List[Parser[A]], st: List[A]): Either[Fail[ParseState[A]], (String, List[A])] =
+    //     ps match {
+    //       case Nil => Right((cs, st))
+    //       case p :: ps => p.run(cs) match {
+    //         case Ok(ParseState(c,s)) => parse(c, ps, s :: st)
+    //         case f@Fail(_) => Left(f)
+    //       }
+    //     }
+    //   parse(input, parsers, List()) match {
+    //     case Right((remaining, parsed)) => Ok(ParseState(remaining, parsed.reverse))
+    //     case Left(Fail(e)) => Fail(e)
+    //   }
+    // }
+
+    // using foldRight
+    Parser {input =>
+      val seed: Either[Fail[ParseState[A]], (String, List[A])] = Right((input, Nil))
+      parsers.foldRight(seed)((p, acc) => acc match {
+        case l@Left(_) => l
+        case Right((in, as)) => p.run(in) match {
+          case Ok(ParseState(rem,parsed)) => Right((rem, parsed :: as))
+          case f@Fail(e)                  => Left(f)
+        }
+      }) match {
+        case Right((rem, parsed)) => Ok(ParseState(rem, parsed.reverse))
+        case Left(Fail(e))        => Fail(e)
+      }
+    }
 
   /**
    * Return a parser that produces the given number of values of
    * the given parser. This fails if the given parser fails in the
    * attempt to produce the given number of values.
    *
-   *
-   * scala> Parser.thisMany(5, Parser.char)).run("hello")
+   *                                   errata: character & extra bracket
+   * scala> Parser.thisMany(5, Parser.character).run("hello")
    *  = Ok(ParseState(,hello))
-   *
-   * scala> Parser.thisMany(6, Parser.char)).run("hello")
+   *  errata: should be Ok(ParseState(,List(h, e, l, l, o)))
+   * scala> Parser.thisMany(6, Parser.character).run("hello")
    *  = Fail(NotEnoughInput)
    */
   def thisMany[A](n: Int, parse: Parser[A]): Parser[List[A]] =
-    ???
+    sequence(List.fill(n)(parse))
 }
 
 /**
@@ -353,6 +400,7 @@ object Parser {
  * Produce a person parser for a record.
  */
 object PersonParser {
+  import Parser._
   /*
    * A data structure representing a person with the following attributes:
    *  - name: non empty string that starts with a capital letter
@@ -373,30 +421,49 @@ object PersonParser {
   /**
    * Parse a name, which is a non-empty string that starts with a capital letter.
    */
-  def nameParser: Parser[String] =
-    ???
+  def nameParser: Parser[String] = for {
+    h <- upper
+    r <- list(alpha)
+  } yield (h :: r).mkString
 
   /**
    * Parse a phone number, which is a string of digits, dots or hyphens that
    * starts with a digit and ends with a hash (#).
    */
-  def phoneParser: Parser[String] =
-    ???
+  def phoneParser: Parser[String] = for {
+    d <- digit
+    r <- list(digit ||| is('.') ||| is('-'))
+    _ <- is('#')
+  } yield (d :: r).mkString
+
+
 
   /**
    * An address is a positive street number and a non empty string for the
    * street name.
    */
-  def addressParser: Parser[Address] =
-    ???
+  def addressParser: Parser[Address] = for {
+    n <- natural
+    _ <- space
+    s <- list(alpha)
+  } yield Address(n, s.mkString)
 
   /**
    * An person record is the following parts, each seperated by one or more spaces.
    *
    *  <name> <age> <phone> <address>
-   */
-  def personParser: Parser[String] =
-    ???
+   */                    //ERRATA wrong return type
+  def spaces1 = list1(space)
+  def personParser: Parser[Person] = for {
+    name    <- nameParser
+    _       <- spaces1
+    age     <- natural
+    _       <- spaces1
+    phone   <- phoneParser
+    _       <- spaces1
+    address <- addressParser
+  } yield Person(name, age, phone, address)
+
 
   /**
    * Parse all records.
@@ -407,13 +474,21 @@ object PersonParser {
    *
    * Hint: Use Parser.sequence
    */
-  def parseAll(data: List[String]): Result[List[Person]] =
-    ???
-
+  def parseAll(data: List[String]): Result[List[Person]] = {
+    val parser = for {
+      p <- personParser
+      _ <- list(is('\n'))
+    } yield p
+    thisMany(data.size, parser).run(data.mkString("\n")) match {
+      case Ok(ParseState(_, persons)) => Ok(persons)
+      case Fail(e)                    => Fail(e)
+    }
+    }
+// ERRATA: names should start with upper case
   def Data = List(
-    "fred 32 123.456-1213# 301 cobblestone"
-  , "barney 31 123.456.1214# 303 cobblestone"
-  , "homer 39 555.123.939# 742 evergreen"
-  , "flanders 39 555.123.939# 744 evergreen"
+    "Fred 32 123.456-1213# 301 cobblestone"
+  , "Barney 31 123.456.1214# 303 cobblestone"
+  , "Homer 39 555.123.939# 742 evergreen"
+  , "Flanders 39 555.123.939# 744 evergreen"
   )
 }
